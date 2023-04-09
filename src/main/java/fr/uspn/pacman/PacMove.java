a79b98988dee8376191c4c1be7ac1fed6d567c07
TRAN Costa <costa.tran@hotmail.com>
2023-04-09 11:33:20 +0200

    Pac-Man moves updated

--- src/main/java/fr/uspn/pacman/PacMove.java
+++ src/main/java/fr/uspn/pacman/PacMove.java
@@ -39,39 +39,127 @@ class PacMove implements KeyListener {
         if (!game.win() || game.getPacman().isAlive()) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_LEFT:
+                    oldDirection = game.getPacman().getDirection();
                     game.getPacman().setDirection(Direction.LEFT);
+                    pacYMove = game.getPacman().getY() + game.getPacman().getDirection().getDy();
+                    if(pacYMove % 36 <= 18 && oldDirection == Direction.UP) {
+                        pacYMove -= pacYMove % 36;
+                        if(map[(pacYMove) / 36][(pacXMove - 1) / 36] != Type.W){
+                            game.getPacman().setY(pacYMove);
+                        }
+                        else {
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                    } 
+                    else if((pacYMove % 36 >= 18 || pacYMove % 36 == 0) && oldDirection == Direction.DOWN){
+                        if(pacYMove % 36 == 0 && map[(pacYMove) / 36][(pacXMove - 1) / 36] != Type.W){
+                        }
+                        else if(map[(pacYMove + 36 - pacXMove % 36) / 36][(pacXMove - 1) / 36] != Type.W){
+                            pacYMove += 36 - pacYMove % 36;
+                            game.getPacman().setY(pacYMove);
+                        }
+                        else{
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                    }
+                    else if (oldDirection == Direction.RIGHT || oldDirection == Direction.VOID){
+                    }
+                    else {
+                        game.getPacman().setDirection(oldDirection);
+                    }
                     break;
+
                 case KeyEvent.VK_RIGHT:
+                    oldDirection = game.getPacman().getDirection();
                     game.getPacman().setDirection(Direction.RIGHT);
+                    pacYMove = game.getPacman().getY() + game.getPacman().getDirection().getDy();
+                    if(pacYMove % 36 <= 18 && oldDirection == Direction.UP){
+                        pacYMove -= pacYMove % 36;
+                        if(map[(pacYMove) / 36][(pacXMove + 37) / 36] != Type.W){
+                            game.getPacman().setY(pacYMove);
+                        }
+                        else {
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                    }
+                    else if ((pacYMove % 36 >= 18 || pacYMove % 36 == 0) && oldDirection == Direction.DOWN){
+                        if(pacYMove % 36 == 0 && map[(pacYMove) / 36][(pacXMove + 37) / 36] != Type.W){
+                        }
+                        else if(map[(pacYMove + 36 - pacXMove % 36) / 36][(pacXMove + 37) / 36] != Type.W){
+                            pacYMove += 36 - pacYMove % 36;
+                            game.getPacman().setY(pacYMove);
+                        }
+                        else{
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                    }
+                    else if (oldDirection == Direction.LEFT || oldDirection == Direction.VOID){
+                    }
+                    else {
+                        game.getPacman().setDirection(oldDirection);
+                    }
                     break;
+
                 case KeyEvent.VK_UP:
                     oldDirection = game.getPacman().getDirection();
                     game.getPacman().setDirection(Direction.UP);
                     pacXMove = game.getPacman().getX() + game.getPacman().getDirection().getDx();
-                    if(pacXMove % 36 < 22 && oldDirection == Direction.LEFT) { 
+                    if(pacXMove % 36 <= 18 && oldDirection == Direction.LEFT){
                         pacXMove -= pacXMove % 36;
-                        if(map[(pacYMove - 34) / 36][(pacXMove) / 36] != Type.W){
+                        if(map[(pacYMove - 1) / 36][(pacXMove) / 36] != Type.W){
                             game.getPacman().setX(pacXMove);
                         }
                         else {
                             game.getPacman().setDirection(oldDirection);
                         }
                     } 
-                    else if (pacXMove % 36 > 14 && oldDirection == Direction.RIGHT){
-                        pacXMove += 36 - (pacXMove % 36);
-                        if(map[(pacYMove - 34) / 36][(pacXMove) / 36] != Type.W){
+                    else if ((pacXMove % 36 >= 18 || pacXMove % 36 == 0) && oldDirection == Direction.RIGHT){
+                        if(pacXMove % 36 == 0 && map[(pacYMove - 1) / 36][(pacXMove) / 36] != Type.W){
+                        }
+                        else if(map[(pacYMove - 1) / 36][(pacXMove + 36 - pacXMove % 36) / 36] != Type.W){
+                            pacXMove += 36 - pacXMove % 36;
                             game.getPacman().setX(pacXMove);
                         }
-                        else {
+                        else{
                             game.getPacman().setDirection(oldDirection);
                         }
+                    }
+                    else if (oldDirection == Direction.DOWN){
                     }
                     else {
                         game.getPacman().setDirection(oldDirection);
                     }
                     break;
+
                 case KeyEvent.VK_DOWN:
+                    oldDirection = game.getPacman().getDirection();
                     game.getPacman().setDirection(Direction.DOWN);
+                    pacXMove = game.getPacman().getX() + game.getPacman().getDirection().getDx();
+                    if(pacXMove % 36 <= 18 && oldDirection == Direction.LEFT){
+                        if(map[(pacYMove + 36) / 36][(pacXMove) / 36] == Type.W){
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                        pacXMove -= pacXMove % 36;
+                        if(map[(pacYMove + 36) / 36][(pacXMove) / 36] != Type.W){
+                            game.getPacman().setX(pacXMove);
+                        }
+                    }
+                    else if ((pacXMove % 36 >= 18 || pacXMove % 36 == 0) && oldDirection == Direction.RIGHT){
+                        if(pacXMove % 36 == 0 && map[(pacYMove + 37) / 36][(pacXMove) / 36] != Type.W){
+                        }
+                        else if(map[(pacYMove + 37) / 36][(pacXMove + 36 - pacXMove % 36) / 36] != Type.W){
+                            pacXMove += 36 - pacXMove % 36;
+                            game.getPacman().setX(pacXMove);
+                        }
+                        else{
+                            game.getPacman().setDirection(oldDirection);
+                        }
+                    }
+                    else if (oldDirection == Direction.UP){
+                    }
+                    else {
+                        game.getPacman().setDirection(oldDirection);
+                    }
                     break;
             }
         }
